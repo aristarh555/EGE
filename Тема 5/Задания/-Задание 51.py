9eb8b271f0daab87b2f9ce09acf01801246ea5fa
Артем <aristarh228228@mail.ru>
2026-01-15 18:37:53 +0300

    Обновлен статус задания 51 темы 5. Задание решено Неверно

---
+++ Тема 5/Задания/-Задание 51.py
@@ -0,0 +1,29 @@
+# Решение
+c = 0
+for n in range(10000000, 100000000000000000000000):
+    bn = bin(n)[2:]
+    y = bin(int(bn, 2) % 3)[2:]
+    bn = str(bn) + str(y)
+    x = bin(int(bn, 2) % 5)[2:]
+    r = str(bn) + str(x)
+    r = int(r, 2)
+    if r < 1444444416 and r > 1111111110:
+        c += 1
+        print(c)
+    elif r > 1444444416:
+        break
+
+
+
+
+
+
+
+
+answer = 694444
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(5, 51, answer, '389499b02f30212486e408cd73a5bc50'))
