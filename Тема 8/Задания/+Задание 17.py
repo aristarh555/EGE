6e7915d6960375ea5a14bf48ab5df4a8427019dd
Андрей Боев <a.boev.it@gmail.com>
2026-01-30 15:58:35 +0300

    NEW: Тема 14

--- Тема 8/Задания/+Задание 17.py
+++ Тема 8/Задания/+Задание 17.py
@@ -3,19 +3,13 @@ from itertools import product
 
 count = 0
 for p in product('2468', '1357', '2468', '1357', '2468', '1357','2468', '1357','2468', '1357', '2468'):
-    if p.count('1') < 4 and p.count('2') < 4 and p.count('3') < 4 and p.count('4') < 4 and p.count('5') < 4 and p.count('6') < 4 and p.count('7') < 4 and p.count('8') < 4:
+    if p.count('1') <= 4 and p.count('2') <= 4 and p.count('3') <= 4 and p.count('4') <= 4 and p.count('5') <= 4 and p.count('6') <= 4 and p.count('7') <= 4 and p.count('8') <= 4:
         count += 1
-# * 2, потому что можно начинать с гласной, а можно и с согласной
 print(count * 2)
-
-
-
-
 
 
 answer = 8200800
 
-#
 
 from tests.conftest import result_register
 if answer is not Ellipsis:
