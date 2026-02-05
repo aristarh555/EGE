08cf63e1b924cd35e57bf29b5b1a5d9e44e31be4
Артем <aristarh228228@mail.ru>
2026-02-05 17:50:47 +0300

    Обновлен статус задания № 21 Тема: 2 > Неверно

--- Тема 2/Задания/-Задание 21.py
+++ Тема 2/Задания/-Задание 21.py
@@ -3,7 +3,7 @@ for x in range(0, 2):
     for y in range(0, 2):
         for z in range(0, 2):
             for w in range(0, 2):
-                if ((x <= y) or (z <= w)) and ((z == y) <= (w == x)) == 0:
+                if (((x <= y) or (z <= w)) and ((z == y) <= (w == x))) == 0:
                     print(x, y, z, w)
 
 
