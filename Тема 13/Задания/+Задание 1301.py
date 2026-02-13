112ad992d1e3071f5d5aed0286699fa01564ada2
Андрей Боев <a.boev.it@gmail.com>
2026-02-13 16:30:34 +0300

    Обновлен статус задания № 1301 Тема: 13 > Верно

--- Тема 13/Задания/+Задание 1301.py
+++ Тема 13/Задания/+Задание 1301.py
@@ -1,10 +1,17 @@
 # Решение
+from ipaddress import ip_network
 
+count = 0
+for mask in range(32, 0, -1):
+    net1 = ip_network(f'157.220.185.237/{mask}', False)
+    net2 = ip_network(f'157.220.184.230/{mask}', False)
+    if net1.network_address == net2.network_address:
+        for ip in net1:
+            if f'{ip:b}'.count('1') == 15:
+                count += 1
+        break
 
-
-
-
-
+print(count)
 
 answer = 9
 
