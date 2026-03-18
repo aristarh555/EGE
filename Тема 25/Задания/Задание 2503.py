bb8885dd515bcf376c27465bcb37bfffc3f80667
Андрей Боев <a.boev.it@gmail.com>
2026-03-18 19:34:00 +0300

    NEW: Тема 25

---
+++ Тема 25/Задания/Задание 2503.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+# Ответ в виде списка чисел []
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(25, 2503, answer, 'c867bc32545e94925e8d2198ad7333d9'))
