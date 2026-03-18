8cd5fc94a5e7b61f2f42df333f32a96b286de1b2
Артем <aristarh228228@mail.ru>
2026-03-18 19:35:39 +0300

    Решение задач

--- Тема 6/Задания/Задание 62.py
+++ Тема 6/Задания/Задание 62.py
@@ -1,6 +1,28 @@
 # Решение
-
-
+from turtle import *
+k=1
+tracer(0)
+rt(180)
+for i in range(9):
+    fd(59*k)
+    lt(90)
+    fd(84*k)
+    lt(90)
+pu()
+fd(18*k)
+lt(90)
+fd(38*k)
+lt(90)
+pd()
+for j in range(9):
+    fd(120*k)
+    rt(90)
+    fd(99*k)
+    rt(90)
+for x in range(-20, 20):
+    for y in range(-20, 20):
+        goto(x, y)
+        dot(3, 'red')
 
 
 
