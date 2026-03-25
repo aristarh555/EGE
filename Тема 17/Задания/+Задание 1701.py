41a269fb724f9cec834a79a520ac8ee4272c88c0
Артем <aristarh228228@mail.ru>
2026-03-25 20:00:47 +0300

    Обновлен статус задания № 1701 Тема: 17 > Верно

---
+++ Тема 17/Задания/+Задание 1701.py
@@ -0,0 +1,16 @@
+# Решение
+
+
+
+
+
+
+
+answer1 = 2627
+answer2 = 504410
+
+#
+
+from tests.conftest import result_register
+if answer1 is not Ellipsis and answer2 is not Ellipsis:
+    print(result_register(17, 1701, f'{answer1} {answer2}', 'b73de31c02e01cfc4508d15f2cf61e69'))
