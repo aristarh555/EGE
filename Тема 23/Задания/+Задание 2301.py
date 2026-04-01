22566ab670d113ed711a5364f04d93599d5f711a
Артем <aristarh228228@mail.ru>
2026-04-01 16:55:46 +0300

    Обновлен статус задания № 2301 Тема: 23 > Верно

---
+++ Тема 23/Задания/+Задание 2301.py
@@ -0,0 +1,22 @@
+# Решение
+def f(x, y):
+    if x == y:
+        return 1
+    if x > y:
+        return 0
+    return f(x + 1, y) + f(x * 2, y) + f(x * 3, y)
+
+print(f(9, 24) * f(24, 27) * f(27, 81))
+print(f(9, 27) * f(27, 81))
+
+
+
+
+
+answer = 142
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(23, 2301, answer, 'a8baa56554f96369ab93e4f3bb068c22'))
