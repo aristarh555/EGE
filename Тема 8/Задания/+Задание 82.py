d3a9d6060fe6c6d7f15cbf7afbb6e55fc4a077e7
Артем <aristarh228228@mail.ru>
2026-04-03 16:34:21 +0300

    Обновлен статус задания № 82 Тема: 8 > Верно

---
+++ Тема 8/Задания/+Задание 82.py
@@ -0,0 +1,23 @@
+# Решение
+from itertools import product
+
+i = 1
+for line in product('АКОРСТ', repeat=5):
+    line = "".join(line)
+    if line[0] not in 'АСТ' and line.count('О') == 2 and i % 2 == 0:
+        print(i)
+    i += 1
+
+
+
+
+
+
+
+answer = 5058
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(8, 82, answer, '7ffb4e0ece07869880d51662a2234143'))
