010e831c9a8c5cafadc565513d72496ec6b4a51d
Артем <aristarh228228@mail.ru>
2026-04-15 15:49:17 +0300

    Обновлен статус задания № 1703 Тема: 17 > Неверно

---
+++ Тема 17/Задания/-Задание 1703.py
@@ -0,0 +1,42 @@
+# Решение
+f = open('Задание 1703.txt', 'r')
+a = f.readlines()
+f.close()
+min_x = 100000
+for i in range(len(a)):
+    a[i] = int(a[i])
+for x in a:
+    if x % 100 // 10 == x % 10:
+        min_x = x
+c = 0
+maxx = -100000
+for i in range(len(a)-1):
+    flag = 0
+    if a[i] % 10 == a[i+1] % 100 // 10 or a[i+1] % 10 == a[i] % 100 % 10:
+        flag += 1
+    if (a[i] % 13 == 0 and a[i+1] % 13 != 0) or (a[i+1] % 13 == 0 and a[i] % 13 != 0):
+        flag += 1
+
+    if a[i]**2 + a[i+1]**2 <= min_x**2:
+        flag += 1
+    if flag == 3:
+        c += 1
+        if a[i]**2 + a[i+1]**2 > maxx:
+            maxx = a[i]**2 + a[i+1]**2
+print(c, maxx)
+
+
+
+
+
+
+
+
+answer1 = 78
+answer2 = 62945424
+
+#
+
+from tests.conftest import result_register
+if answer1 is not Ellipsis and answer2 is not Ellipsis:
+    print(result_register(17, 1703, f'{answer1} {answer2}', '1b1a3f384b8b6bddea7c6e63fad46024'))
