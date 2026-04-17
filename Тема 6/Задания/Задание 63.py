3f451870e2d50f9f8fcb3e0c43975f6e33c9f12e
Артем <aristarh228228@mail.ru>
2026-04-17 15:33:32 +0300

    Решение задач

--- Тема 6/Задания/Задание 63.py
+++ Тема 6/Задания/Задание 63.py
@@ -1,5 +1,28 @@
 # Решение
-
+from turtle import *
+tracer(-1)
+k = 5
+for i in range(7):
+    fd(17*k)
+    rt(90)
+    fd(26*k)
+    rt(90)
+pu()
+fd(4*k)
+rt(90)
+fd(6*k)
+lt(90)
+pd()
+for i in range(7):
+    fd(278*k)
+    rt(90)
+    fd(345*k)
+    rt(90)
+pu()
+for x in range(0, 350):
+    for y in range(0, 350):
+        goto(x*k, y*k)
+        dot(3, 'red')
 
 
 
