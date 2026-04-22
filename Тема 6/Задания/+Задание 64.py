06bc5e10cc9790987c9ff3af65e9158ca72d2d47
Артем <aristarh228228@mail.ru>
2026-04-22 19:10:22 +0300

    Обновлен статус задания № 64 Тема: 6 > Верно

---
+++ Тема 6/Задания/+Задание 64.py
@@ -0,0 +1,27 @@
+# Решение
+from turtle import *
+k = 10
+tracer(0)
+
+for i in range(2):
+    fd(15*k)
+    rt(90)
+    fd(8*k)
+    rt(90)
+pu()
+for x in range(-20, 100):
+    for y in range(-20, 100):
+        goto(x*k, y*k)
+        dot(3, 'red')
+exitonclick()
+
+
+
+
+answer = 56
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(6, 64, answer, '9f61408e3afb633e50cdf1b20de6f466'))
