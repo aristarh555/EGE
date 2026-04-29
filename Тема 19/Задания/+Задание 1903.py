976d20bdf23cefc351df4f90378d61a68000d928
Артем <aristarh228228@mail.ru>
2026-04-29 19:58:22 +0300

    Обновлен статус задания № 1903 Тема: 19 > Верно

---
+++ Тема 19/Задания/+Задание 1903.py
@@ -0,0 +1,27 @@
+# Решение
+def game(heap, moves, to):
+    if heap < 19:
+        return moves % 2 == to % 2
+    if moves == to:
+        return 0
+    h = [game(heap-5, moves+1, to)]
+    if heap % 2 == 0:
+        h.append(game(heap//2, moves+1, to))
+    if heap % 3 == 0:
+        h.append(game(heap//3, moves+1, to))
+    if heap % 2 != 0 and heap % 3 != 0:
+        h.append(game(heap+1, moves+1, to))
+    return any(h) if (moves + 1) % 2 == to % 2 else all(h)
+print(min(s for s in range(19, 10000) if not game(s, 0, 1) and game(s, 0, 2)))
+
+
+
+
+
+answer = 25
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(19, 1903, answer, '8e296a067a37563370ded05f5a3bf3ec'))
