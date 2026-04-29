a4eaa13075a80d45363e7b0efaf148748f45aa1e
Андрей Боев <a.boev.it@gmail.com>
2026-04-29 19:34:45 +0300

    Проверка ДЗ

--- Тема 5/Задания/+Задание 52.py
+++ Тема 5/Задания/+Задание 52.py
@@ -1,13 +1,17 @@
 # Решение
 for n in range(2, 1000000):
     r = bin(n)[2:]
+    c = r[0::2].count('0')
+    k = r[1::2].count('1')
+    '''
     c = 0
     k = 0
-    for i in range(0, len(r)):
+    for i in range(len(r)):
         if i % 2 == 0 and r[i] == '0':
             c += 1
         if i % 2 != 0 and r[i] == '1':
             k += 1
+    '''
     r = abs(c - k)
     if r == 5:
         print(n)
