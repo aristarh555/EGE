6fa1dbf0ba93bce96e6daeef3e319d91039734d4
Андрей Боев <a.boev.it@gmail.com>
2026-05-03 23:42:49 +0300

    NEW: 1904

---
+++ Тема 19/Задания/Задание 1904.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = ...
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(19, 1904, answer, '543a84894716c6c0ed6cabe60aac9945'))
