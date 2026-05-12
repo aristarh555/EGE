9886226bdf0b95029e372006a061fc2db9f3fe06
Артем <aristarh228228@mail.ru>
2026-05-12 19:42:39 +0300

    Обновлен статус задания № 75 Тема: 7 > Неверно

---
+++ Тема 7/Задания/-Задание 75.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = 16.5
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(7, 75, answer, '0e65972dce68dad4d52d063967f0a705'))
