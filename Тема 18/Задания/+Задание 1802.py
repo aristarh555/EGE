d8a55953e8666522c2b04d292af0904d78cee7fa
Артем <aristarh228228@mail.ru>
2026-05-17 19:19:55 +0300

    Обновлен статус задания № 1802 Тема: 18 > Верно

---
+++ Тема 18/Задания/+Задание 1802.py
@@ -0,0 +1,16 @@
+# Решение
+
+
+
+
+
+
+
+answer1 = 6653
+answer2 = 2522
+
+#
+
+from tests.conftest import result_register
+if answer1 is not Ellipsis and answer2 is not Ellipsis:
+    print(result_register(18, 1802, f'{answer1} {answer2}', 'b81e90ef85a5fd954a9dc25f22103bd0'))
