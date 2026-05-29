1065580f42fe58e2fa5b8765269695293ff02ff0
Андрей Боев <a.boev.it@gmail.com>
2026-05-29 16:20:54 +0300

    NEW: Тема 26

---
+++ Тема 26/Задания/Задание 2601.py
@@ -0,0 +1,14 @@
+# Решение
+
+
+
+
+
+answer1 = ...
+answer2 = ...
+
+#
+
+from tests.conftest import result_register
+if answer1 is not Ellipsis and answer2 is not Ellipsis:
+    print(result_register(26, 2601, f'{answer1} {answer2}', '74d369f56d75ff373745b980cf3be919'))
