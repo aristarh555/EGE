c2065a565f40df524eae0eae5cedcc9d6f95bd0f
Артем <aristarh228228@mail.ru>
2026-05-30 19:28:54 +0300

    Обновлен статус задания № 114 Тема: 11 > Верно

---
+++ Тема 11/Задания/+Задание 114.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = 4096
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(11, 114, answer, 'f7efa4f864ae9b88d43527f4b14f750f'))
