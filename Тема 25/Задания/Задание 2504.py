26179a13708055a74bf9fdbd83bcc4a8a34359cb
Артем <aristarh228228@mail.ru>
2026-06-10 16:07:27 +0300

    Решение задач

--- Тема 25/Задания/Задание 2504.py
+++ Тема 25/Задания/Задание 2504.py
@@ -1,5 +1,23 @@
 # Решение
+def get_divs(n):
+    divs = set()
+    for div in range(2, int(n ** 0.5) + 1):
+        if n % div == 0:
+            divs.add(div)
+            divs.add(n // div)
+    return sorted(divs)
 
+def prost(n):
+    for j in range(2, int(n ** 0.5) + 1):
+        if n % j == 0:
+            return False
+    return True
+def prost_divs(n):
+    l = []
+    for i in get_divs(n):
+        if prost(i):
+            l.append(i)
+    return l
 
 
 
