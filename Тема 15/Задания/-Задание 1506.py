41bce4aed987dd3e23c4b3b8344f7d99abe65d69
Артем <aristarh228228@mail.ru>
2026-06-10 16:43:29 +0300

    Обновлен статус задания № 1506 Тема: 15 > Неверно

--- Тема 15/Задания/-Задание 1506.py
+++ Тема 15/Задания/-Задание 1506.py
@@ -1,12 +1,22 @@
 # Решение
+'''
+p = range(167242, 514210)
+q = range(403149, 718530)
+r = range(522897, 816282)
+min_len = 1000000
+for begin in range(800000):
+    for end in range(800000):
+        a = range(begin, end)
+        if all((x in q) <= (((x in p) or (x in r)) <= (x in a)) for x in range(800000)):
+            min_len = min(min_len, end - begin)
+print(min_len)
+'''
 
 
 
 
 
-
-
-answer = 8688
+answer = 111062
 
 #
 
