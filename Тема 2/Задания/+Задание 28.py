8b1fc490d55c6858d18b02131f16d2c45e29ffd1
Артем <aristarh228228@mail.ru>
2026-06-14 12:38:47 +0300

    Обновлен статус задания № 28 Тема: 2 > Верно

---
+++ Тема 2/Задания/+Задание 28.py
@@ -0,0 +1,11 @@
+# Решение
+
+
+
+answer = 'xwyz'
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(2, 28, answer, 'b38b40578b2b263db0a76f0db6771104'))
