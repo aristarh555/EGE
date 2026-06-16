c27fc6f41c8bd3fd3aaa5bacadec66deac5a2816
Артем <aristarh228228@mail.ru>
2026-06-16 11:39:50 +0300

    Обновлен статус задания № 2504 Тема: 25 > Неверно

---
+++ Тема 25/Задания/-Задание 2504.py
@@ -0,0 +1,42 @@
+# Решение
+def get_divs(n):
+    divs = set()
+    for div in range(2, int(n ** 0.5) + 1):
+        if n % div == 0:
+            divs.add(div)
+            divs.add(n // div)
+    return sorted(divs)
+
+def prost(n):
+    for j in range(2, int(n ** 0.5) + 1):
+        if n % j == 0:
+            return False
+    return True
+def prost_divs(n):
+    l = []
+    for i in get_divs(n):
+        if prost(i):
+            l.append(i)
+    return l
+l = []
+for i in range(4555705, 1000000000):
+    if i % 10 != 3:
+        x = i - (sum(prost_divs(i)) + len(prost_divs(i)))
+        if x % 100 == 23:
+            if i == sum(prost_divs(i)) + len(prost_divs(i)) + x:
+                l.append(i)
+                print(i, x)
+    if len(l) == 5:
+        print(l)
+        break
+
+
+
+# Ответ в виде списка чисел []
+answer = [4555721, 4555745, 4555755, 4555846, 4555965]
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(25, 2504, answer, '4f6cb100db828c27d436322ae3bffeef'))
