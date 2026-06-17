228cbe3a457d18aa2aa106153c028cff90c940aa
Андрей Боев <a.boev.it@gmail.com>
2026-06-17 14:23:31 +0300

    NEW: Тема 25 > Задание 2506

---
+++ Тема 25/Задания/Задание 2506.py
@@ -0,0 +1,18 @@
+# Решение
+
+
+
+
+
+
+# Ответ в виде списка чисел []
+# 1й столбец
+answer1 = ...
+# 2й столбец
+answer2 = ...
+
+#
+
+from tests.conftest import result_register
+if answer1 is not Ellipsis and answer2 is not Ellipsis:
+    print(result_register(25, 2506, answer1 + answer2, 'e6da587657bbbab0d1ca022034492d03'))
